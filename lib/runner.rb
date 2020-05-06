40f82a14bf1470a0ba024be72995bb3a98e89f14
rwilt <rosie.wilt@gmail.com>
2020-05-06 10:21:20 -0400

    updated cli

---
+++ lib/runner.rb
@@ -0,0 +1,8 @@
+require 'asciiart'
+require 'rmagick'
+
+a = AsciiArt.new("https://upload.wikimedia.org/wikipedia/commons/d/d7/Meisje_met_de_parel.jpg")
+  #<AsciiArt:0x00000100878678 @file=#<File:/Users/sschor/Desktop/uncle_larry.jpg>>
+ puts a.to_ascii_art(color: true, width:100)
+
+puts "Terminal Art Gallery"
